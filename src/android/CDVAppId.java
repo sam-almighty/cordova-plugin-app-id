0db882406da92e988f0d2b29009ef1df15f91ff9
sam-almighty <sam.almighty85@gmail.com>
2018-10-24 12:30:18 +0530

    Updated plugin

--- src/android/CDVAppId.java
+++ src/android/CDVAppId.java
@@ -72,7 +72,7 @@ public class CDVAppId extends CordovaPlugin {
             if (storedRefreshToken != null && !storedRefreshToken.isEmpty()) {
                 refreshTokens(storedRefreshToken,callbackContext);
             } else {
-                callbackContext.error("");
+               // callbackContext.error("");
             }
     	} catch (Exception e) {
     		callbackContext.error(e.toString());
